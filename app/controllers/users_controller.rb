998244488f0737296b24a5eb8f92c4782f8503d7
EC2 Default User <ec2-user@ip-172-31-31-2.us-east-2.compute.internal>
2019-03-06 13:26:05 +0000

    9章　月間表示切り替え　完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @first_day = Date.today.beginning_of_month
-    @last_day = @first_day.end_of_month
+    if params[:first_day].nil?
+      @first_day = Date.today.beginning_of_month
+    else
+      @first_day = Date.parse(params[:first_day])
+    end
+  @last_day = @first_day.end_of_month
     (@first_day..@last_day).each do |day|
       unless @user.attendances.any? {|attendance| attendance.worked_on == day}
       record = @user.attendances.build(worked_on: day)
